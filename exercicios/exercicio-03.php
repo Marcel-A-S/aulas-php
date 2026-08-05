e600b13f4988d2becea42bb2b441053408bc80c5
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-05 10:45:05 -0300

    Exercício 03 Condicionais PHP interligado com HTML

---
+++ exercicios/exercicio-03.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Exercicio 03 </title>
+
+</head>
+
+<body>
+
+    <h1> Condicionais e uso do PHP intercalando com HTML</h1>
+
+    <hr>
+
+    <?php
+
+    $idade = 70;
+
+    if ($idade < 12) :
+        $tipoPessoa = "infantil";
+        $valorDoIngresso = 25.00;
+
+    elseif ($idade <= 60) :
+        $tipoPessoa = "idoso";
+        $valorDoIngresso = 40.00;
+
+    else:
+        $tipoPessoa = "adulto";
+        $valorDoIngresso = 20.00;
+
+    endif;
+
+    ?>
+
+    <h2> Ingressos para o Show</h2>
+
+    <p> <?=  $tipoPessoa ?> é <?=  $valorDoIngresso ?></p>
+    
+
+
+
+
+
+
+</body>
+
+</html>
