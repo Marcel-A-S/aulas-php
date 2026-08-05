4b0f071d75745b3de55b75744a3ac3b02d3511d9
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-05 11:53:14 -0300

    comando de repetição loop revisado

--- 09-loop.php
+++ 09-loop.php
@@ -31,9 +31,57 @@ endwhile; // ou }
 
 
 
+<hr>
 
 
+<h2>do/while (faça/enquanto)</h2>
+<p>Executa ações pelo menos <b> uma vez</b> e´se a condição for verdadeira, continua executando outras vezes</p>
 
-    
+
+<?php 
+
+$j = 1;
+do{
+?>
+
+<div>
+<h3> Título qualquer...</h3>
+<p>Bloco <?= $j ?></p>
+
+</div>
+
+
+<?php 
+$j++;
+}while ($j <= 3)
+?>
+
+
+<hr>
+
+
+<h2> for (para)</h2>
+
+<p>Executa ações de acordo com uma <b>quantidade determinada de vezes</b>.</p>
+
+
+<section>
+
+<h3>Conteudo da seção</h3>
+
+<?php for($i = 1; $i <= 5; $i++){ ?>
+
+<details>
+ 
+<summary>Pergunta <?=  $i ?> </summary>
+<p>Resposta <?=  $i ?></p>
+
+</details>
+
+<?php } ?>
+
+</section>
+
+   
 </body>
 </html>
