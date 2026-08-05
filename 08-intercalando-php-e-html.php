7e6e0a4c6da4c20ce3e3f75a5bbd1ae962d89007
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-05 09:06:35 -0300

    Intercalando PHP e HTML

---
+++ 08-intercalando-php-e-html.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisando PHP intercalado com HTML</title>
+
+</head>
+<body>
+    
+
+<h1> Revisando PHP intercalado com HTML </h1>
+
+<hr>
+
+<?php 
+
+$aluno = "Fulano";
+const ESCOLA = "Senac Penha";
+
+echo "<p> O aluno se chama $aluno</p>";
+echo "<p> Ele estuda na escola ".ESCOLA."</p>";
+
+?>
+
+ <h2> Usando PHP intercalando com HTML </h2>
+ <p><i>(Usando o PHP onde precisa)</i> </p>
+<p> O aluno estuda na escola <?= ESCOLA ?></p>
+
+<hr>
+
+<h2> Usando PHP intercalando comandos com HTML </h2>
+
+<?php  $idade = 25; ?> <!-- PHP onde preciso -->
+
+<h3> Resultado (usando PHP pra tudo):</h3>
+<?php
+if($idade >= 18){
+    echo "<p><b>$aluno</b> é maior de idade </p>";
+
+}else{
+    echo "<p><i>$aluno</i> é menor de idade </p>";
+}
+
+?>
+
+<h3> Resultado (Usando PHP só onde é necessário):</h3>
+<?php 
+if($idade >= 18){
+?>
+
+<p><b><?=  $aluno ?></b> é maior de idade</p>
+<?php
+}else {
+    ?>
+<p><i><?=  $aluno ?></i> é menor de idade</p>
+
+<?php
+}
+
+
+?>
+
+
+
+
+
+    
+</body>
+</html>
