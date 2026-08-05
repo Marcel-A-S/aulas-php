763028f27e2a75c1b6be786264ab4bb305c3dc3a
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-05 11:25:08 -0300

    exercicio revisado

--- exercicios/exercicio-03.php
+++ exercicios/exercicio-03.php
@@ -6,6 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Exercicio 03 </title>
 
+    <style>
+        section {
+
+            padding: 4px;
+            margin: auto;
+            width: 50%;
+            border: solid 1px;
+
+
+        }
+
+        .infantil {
+            background-color: pink;
+        }
+
+        .adulto {
+            background-color: greenyellow;
+        }
+
+        .idoso {
+            background-color: plum;
+        }
+    </style>
+
+
+
 </head>
 
 <body>
@@ -16,31 +42,39 @@
 
     <?php
 
-    $idade = 9;
+    $idade = 10;
 
     if ($idade < 12) :
         $tipoPessoa = "infantil";
         $valorDoIngresso = 25.00;
+        $estilo = "infantil";
 
     elseif ($idade <= 60) :
         $tipoPessoa = "idoso";
         $valorDoIngresso = 40.00;
+        $estilo = "idoso";
 
     else:
         $tipoPessoa = "adulto";
         $valorDoIngresso = 20.00;
+        $estilo = "adulto";
 
     endif;
 
     ?>
 
-    <h2> Ingressos para o Show</h2>
-
-    <p> <?=  $tipoPessoa ?> é <?=  $valorDoIngresso ?></p>
-    <p>Preço: R$ <?=  number_format($valorDoIngresso, 2, ",",".")  ?></p>
-    
 
 
+    <section class="<?= $estilo ?>">
+
+
+        <h2> Ingressos para o Show</h2>
+
+        <p> <?= $tipoPessoa ?> é <?= $valorDoIngresso ?></p>
+        <p>Preço: R$ <?= number_format($valorDoIngresso, 2, ",", ".")  ?></p>
+
+
+    </section>
 
 
 
