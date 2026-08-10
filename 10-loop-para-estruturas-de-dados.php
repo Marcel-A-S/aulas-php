dcb375171b757579dd1c488321f95a74aa6226e1
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-10 10:34:24 -0300

    Usando o Loop for para acessar uma matriz

--- 10-loop-para-estruturas-de-dados.php
+++ 10-loop-para-estruturas-de-dados.php
@@ -1,47 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> PHP Loops para estruturas</title>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 
 </head>
+
 <body>
 
-<div class="container"> 
-    
-<h1> Loops para estruturas de dados</h1>
+    <div class="container">
 
-<hr>
+        <h1> Loops para estruturas de dados</h1>
 
-<?php 
+        <hr>
 
-$meses = ["Janeiro", "Fevereiro", "Março",
-          "Abril", "Maio", "Junho"];
+        <?php
 
-?>
+        $meses = [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho"
+        ];
 
-<h2> Usando o loop for para acessar o array</h2>
+        ?>
 
-<ol>
+        <h2> Usando o loop for para acessar o array</h2>
 
-
-<?php for($i = 0; $i <  count($meses); $i++){ ?>
-          <li> <?=  $meses[$i] ?></li>
-          <?php } ?>
-
-</ol>
-
-</div>
+        <ol>
 
 
+            <?php for ($i = 0; $i <  count($meses); $i++) { ?>
+                <li> <?= $meses[$i] ?></li>
+            <?php } ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        </ol>
+
+        <hr>
+
+        <h2> Usando o Loop for para acessar uma matriz
+            (array de arrays)</h2>
+
+        <?php
+
+        $planoDeEstudos = [
+
+            ["Js Avançado", "Node.Js", "Nest.Js"],
+            ["PHP", "Orientação a Objetos"],
+            ["Teoria das Cores", "Photoshop com IA", "UX/UI"]
+
+        ];
+
+        $linhas = count($planoDeEstudos);
+        for ($i = 0; $i < $linhas; $i++): // acesar cada linha
+            $colunas = count($planoDeEstudos[$i]);
+            for ($j = 0; $j < $colunas; $j++): // acessar cada coluna
+        ?>
+
+                <p> <?= $planoDeEstudos[$i][$j] ?></p>
+
+        <?php
+            endfor; // fim do acesso a cada coluna
+        endfor; // fim do acesso a cada linha
 
 
-    
+
+        ?>
+
+
+
+
+
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
+
 </body>
+
 </html>
