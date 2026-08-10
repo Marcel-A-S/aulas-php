3c76a12aad099ff0aee9b00709b17dc0176e5338
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-10 11:52:37 -0300

    exercício 04 Identificadores numérico

---
+++ exercicios/exercicio-04.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 04 Estruturas de repetição</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container">
+
+
+<h1>Identificadores numérico</h1>
+
+<hr>
+
+<?php 
+
+$linguagem =[
+
+      [1 => "HTML", "estruturação da página web" ],
+      [2 => "CSS",  "Estilo da vida na página web"],
+      [3 => "JS", "Back-End aplicação e processamento de dados"],
+      [4 =>"SQL", "Linguagem de Consulta estruturada" ]
+
+]
+
+    
+?>
+
+
+</div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
+    
+</body>
+</html>
