81f2b10081ac4fcadf2832cf8f470d2d2ca328ce
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-12 09:31:16 -0300

    Exercicio 04 adicionando tabela no php

--- exercicios/exercicio-04.php
+++ exercicios/exercicio-04.php
@@ -1,41 +1,97 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 04 Estruturas de repetição</title>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+
 <body>
 
-<div class="container">
+    <div class="container">
 
 
-<h1>Identificadores numérico</h1>
+        <h1>Identificadores numérico</h1>
 
-<hr>
+        <hr>
 
-<?php 
+        <?php
 
-$linguagem =[
+        $linguagens = [
 
-      [1 => "HTML", "estruturação da página web" ],
-      [2 => "CSS",  "Estilo da vida na página web"],
-      [3 => "JS", "Back-End aplicação e processamento de dados"],
-      [4 =>"SQL", "Linguagem de Consulta estruturada" ],
-
-
-      
-   
-]
-
-    
-?>
+            [
+                "id" => 1,
+                "nome" => "HTML",
+                "descricao" => "estruturação da página web"
+            ],
 
 
-</div>
+            [
+                "id" => 2,
+                "nome" => "CSS",
+                "descricao" => "Estilo da vida na página web"
+            ],
+
+
+            [
+                "id" => 3,
+                "nome" => "JS",
+                "descricao" => "Back-End aplicação e processamento"
+            ],
+
+
+            [
+                "id" => 4,
+                "nome" => "SQL",
+                "descricao" => "Linguagem de Consulta estruturada"
+            ],
+
+        ];
+
+        ?>
+
+
+        <?php
+        foreach ($linguagens as $linguagem): // lembrar de colocar no singular
+
+        ?>
+
+            <p>
+                <b> <?= $linguagem["id"] ?> </b>
+                <?= $linguagem["nome"] ?>
+                <?= $linguagem["descricao"] ?>
+            </p>
+        <?php
+        endforeach;
+
+        ?>
+
+        <table>
+            <tr>
+                <th> ID</th>
+                <th>Linguagem</th>
+                <th>Descrição</th>
+            </tr>
+
+            <?php foreach ($linguagens as $linguagem): ?>
+
+                <tr>
+                    <td><?= $linguagem['id'] ?></td>
+                    <td><?= $linguagem['nome'] ?></td>
+                    <td><?= $linguagem['descricao'] ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+
+        </table>
+
+
+
+    </div>
 
 
 
@@ -43,6 +99,7 @@ $linguagem =[
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 
-    
+
 </body>
+
 </html>
