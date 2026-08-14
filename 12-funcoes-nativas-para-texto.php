ba4486b390ede316c2056e9fe55cc07a147204b0
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-14 11:05:06 -0300

    12-funcoes-nativas-para-texto.php

---
+++ 12-funcoes-nativas-para-texto.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas para texto</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<div class=" conteiner">
+
+<h1> Funções nativas para texto</h1>
+<hr>
+
+<h2> mb_strlen()</h2>
+<?php 
+
+
+$texto = "Uma frase qualquer, com acento e cedilha: ação, ciência";
+
+?>
+
+<p>String do exemplo:  <?=  $texto  ?></p>
+<p>Tamanho da string:  <?=  mb_strlen($texto) ?></p>
+
+
+<h2>mb_strtoupper()</h2>
+<p>Conversão para maiúsculas: <?= mb_strtoupper($texto) ?></p>
+
+
+<h2>mb_strtolower()</h2>
+<p>Conversão para minúculas: <?= mb_strtolower($texto) ?></p>
+
+
+<h2>str_replace() ou str_ireplace</h2>
+
+<?php 
+
+$frase = "Esta é uma frase com palavras feias, como burro, idiota, chato demais e outras palavras ruins (bobo,panaca etc)! Chato mesmo! BOBO pra caramba. Também é um BOBÃO";
+
+// Procurar por UMA palavra e substituirndo por outra
+$fraseComSubstituicaoDePalavra = str_ireplace("bobo", "cara legal", $frase);
+
+?>
+
+<p><mark>Frase original: <?= $frase ?></mark></p>
+<p>Frase com substituição de palavras: <?= $fraseComSubstituicaoDePalavra ?></p>
+
+</div>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    
+</body>
+</html>
--- exercicios/exercicio-05.php
+++ exercicios/exercicio-05.php
@@ -6,21 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções</title>
 
-<style>
-
-body{
-
-background:
-padding: 2px;
-
-
-}
-
-
-</style>
-
-
-
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
@@ -31,19 +16,23 @@ padding: 2px;
 
     <h1>Variáveis para representar notas de um aluno. </h1>
 
+     
     <?php
+
+    // dados de entrada
     $nota1 = 8;
     $nota2 = 7;
     $nota3 = 6;
 
-
-    function calcularMedia($nota1, $nota2, $nota3)
+// function calcularMedia( float $n1, float $n2, float $n3)
+    function calcularMedia($n1, $n2, $n3)
     {
          $media = ($nota1 + $nota2 + $nota3) / 3;
+         
         return number_format($media, 1, ',', '.');
     }
 
-
+    // function verificarSituacao( float$): string{
     function verificarSituacao($media)
     {
         if ($media >= 7) {
@@ -76,7 +65,11 @@ if ($media >= 7) {
     <p>nota 2: <?= $nota2 ?></p>
     <p>nota 3: <?= $nota3 ?></p>
 
+    <h2>resultado</h2>
+
     <p>média: <?= ($media) ?></p>
+
+    
 
     <h2 class="<?= $classe ?>">Situação do aluno: <?= $situacao ?></h2>
 
