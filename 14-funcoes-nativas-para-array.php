706b0657ac44687f0330af94f05cc06be0b885a0
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-17 11:53:09 -0300

    Array_combine

--- 14-funcoes-nativas-para-array.php
+++ 14-funcoes-nativas-para-array.php
@@ -1,69 +1,159 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções para Array</title>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+
 <body>
 
-<div class="container">
+    <div class="container">
 
-<h1> Funções nativas para arrays</h1>
-<hr>
-<h2>implode()</h2>
-<p>Transfoma array em uma string.</p>
+        <h1> Funções nativas para arrays</h1>
+        <hr>
+        <h2>implode()</h2>
+        <p>Transfoma array em uma string.</p>
+
+        <?php
+        $arrayBandas = ["Pink Floyd", "Genesis", "Yes"];
+        $textoBandas = implode("-, $arrayBandas");
+
+        ?>
+
+        <pre><?php var_dump($arrayBandas) ?></pre>
+        <pre><?php var_dump($textoBandas) ?></pre>
+
+        <hr>
+
+        <h2>extract()</h2>
+        <p>Extrai chaves associativas para variáveis.</p>
+
+        <?php
+
+        $nome = "Beltrano";
+
+
+
+        $aluno = ["id" => 1, "nome" => "Fulano", "idade" => 25];
+        extract($aluno, EXTR_PREFIX_ALL, "chave");
+
+        // Usamos o segundo parâmetro para definir um prefixo para os nomes
+        // Isso evita conflito/sobreescrita de outras variáveis.
+
+
+        ?>
+
+        <ul>
+
+            <li>ID: <?= $id ?></li>
+            <li>Nome: <?= $nome ?></li>
+            <li>Idade: <?= $idade ?></li>
+
+        </ul>
+
+        <p>Variável <code>$nome</code><?= $nome ?></p>
+
+        <hr>
+
+        <h2>Array_sum()</h2>
+        <p>Somando os valores de um array</p>
+
+        <?php
+        $carrinhoDeCompras = [
+            "TV_Led" => 1200,
+            "Ultrabook" => 2500,
+            "Geladeira" => 3000,
+        ];
+
+        $total = array_sum($carrinhoDeCompras);
+
+        ?>
+
+        <p>Total: <?= $total ?></p>
+
+        <hr>
+
+        <h2>Array_unique()</h2>
+        <p>Gera um novo array removendo elementos duplicados/repetidos em um array.</p>
+
+        <?php
+        $categorias = [
+            "Eletrônicos",
+            "livros",
+            "Roupas",
+            "livros",
+            "Games",
+            "Eletrônicos"
+        ];
+
+        $categoriasUnicas = array_unique($categorias);
+        ?>
+
+        <pre><?php var_dump($categorias) ?></pre>
+        <pre><?php var_dump($categoriasUnicas) ?></pre>
+        <hr>
+
+
+        <h2>Array_merge()</h2>
+        <p>Junta dados de arrays diferentes.</p>
+
+        <?php
+        $produtosFilialNorte = ["Mouse", "Teclado"];
+        $produtosFilialSul = ["Monitor", "Webcam", "Pendrive"];
+
+
+        // $produtos = array_merge($produtosFilialNorte, $produtosFilialSul);
+        // Podemos combinar funções de array (abaixo, merge e depois unique)
+
+        $produtos = array_unique(
+            $produtos = array_merge($produtosFilialNorte, $produtosFilialSul)
+        );
+
+        ?>
+
+        <pre><?php var_dump($produtos) ?></pre>
+
+        <hr>
+
+        <h2>Array_combine()</h2>
+        <p>Cria um novo array a partir de uma lista de valores e uma lista de chaves.</p>
+
+        <?php 
+
+        //Lista de chaves
+        $games = ["super_Mario", "Sonic", "Final_Fantasy"];
+        
+        // Lista de valores
+        $precos = [99, 50, 129];
+
+
+        $catalogo = array_combine($games, $precos);    
+        ?>
+
+        <pre><?php  var_dump($catalogo) ?></pre>
+        <hr>
+
+
+        <h2>Array_map()</h2>
+
+        <p>Percorre cada elemento de um array, executa uma função (chamada de callback) e gera um novo array com os resultados.</p>
 
 <?php 
-$arrayBandas = ["Pink Floyd", "Genesis", "Yes"];
-$textoBandas = implode("-, $arrayBandas");
-
-?> 
-
-<pre><?php var_dump($arrayBandas) ?></pre>
-<pre><?php var_dump($textoBandas) ?></pre>
-
-<hr>
-
-<h2>extract()</h2>
-<p>Extrai chaves associativas para variáveis.</p>
-
-<?php 
-
-$nome = "Beltrano";
-
-
-
-$aluno = ["id" => 1, "nome" => "Fulano", "idade" => 25];
-extract($aluno, EXTR_PREFIX_ALL, "chave");
-
-// Usamos o segundo parâmetro para definir um prefixo para os nomes
-// Isso evita conflito/sobreescrita de outras variáveis.
-
+$catalogoComDesconto = array_map(function($preco){
+return $preco - $preco * 0.10;
+}, $catalogo);
 
 ?>
-
-<ul>
-
-<li>ID: <?= $id ?></li>
-<li>Nome: <?= $nome ?></li>
-<li>Idade: <?= $idade ?></li>
-
-</ul>
-
-<p>Variável <code>$nome</code><?= $nome ?></p>
+    </div>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
-
-
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    
 </body>
+
 </html>
