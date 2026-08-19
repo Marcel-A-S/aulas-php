090f3bab50b34844308cbb21e0ebb40f2458d552
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-19 11:10:27 -0300

    Lista html

--- 16-inclusao-de-recursos.php
+++ 16-inclusao-de-recursos.php
@@ -1,6 +1,7 @@
 <?php include "recursos.php"; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,33 +10,44 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+
 <body>
 
-<div class="conteiner">
+    <div class="conteiner">
 
-<h1>Inclusão der Recursos</h1>
-<hr>
+        <h1>Inclusão der Recursos</h1>
+        <hr>
 
-<p>Utilizado os comandos <code>include</code> e/ou <code>require</code> para importar arquivos com recursos de qualquer natureza,permitindo assim a reutilização de código.</p>
-
-
-<h2>Exemplos de uso/acesso</h2>
-<p>Estamos estudando no <?= ESCOLA ?> fazendo o curso <?= $curso ?>.</p>
-<p>Para fazer o curso o aluno deve ser maior idade.</p>
-<p>Como você <?= ALUNO ?> tem 20 anos, você é <?= verificarIdade(20) ?></p>
+        <p>Utilizado os comandos <code>include</code> e/ou <code>require</code> para importar arquivos com recursos de qualquer natureza,permitindo assim a reutilização de código.</p>
 
 
-<h2>Exemplo de carregamento de conteúdo parcial/fragmento</h2>
-
-<section>
-
-<h3>Título qualquer</h3>
-<?php include "textos.php"; ?>
-
-</section>
+        <h2>Exemplos de uso/acesso</h2>
+        <p>Estamos estudando no <?= ESCOLA ?> fazendo o curso <?= $curso ?>.</p>
+        <p>Para fazer o curso o aluno deve ser maior idade.</p>
+        <p>Como você <?= ALUNO ?> tem 20 anos, você é <?= verificarIdade(20) ?></p>
 
 
-</div>
+        <h2>Exemplo de carregamento de conteúdo parcial/fragmento</h2>
+
+        <section>
+
+            <h3>Título qualquer</h3>
+            <?php include "textos.php"; ?>
+
+            <ul>
+                <?php foreach ($tecnologias as $tecnologia): ?>
+                    <li><?= $tecnologia ?></li>
+                <?php endforeach ?>
+            </ul>
+
+
+        </section>
+
+
+
+
+
+    </div>
 
 
 
@@ -44,7 +56,8 @@
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
--- textos.php
+++ textos.php
@@ -1,6 +1,9 @@
+<!-- texto.php -->
+
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione voluptatem porro exercitationem sunt non nemo adipisci eveniet ex, quasi quod quas blanditiis maxime unde voluptate ipsam voluptas tenetur at voluptates!
 </p>
 
+<?php  include "lista.html"; ?>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis autem quo, deleniti vel recusandae voluptate ad nemo voluptatibus provident at beatae neque odit, hic ullam nulla deserunt? Sint, aspernatur temporibus!</p>
 
