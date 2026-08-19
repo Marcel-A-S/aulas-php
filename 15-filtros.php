d87db0ee98981c6412769dee344d5e74cb9ad804
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-19 10:00:20 -0300

    funções  alternativas

--- 15-filtros.php
+++ 15-filtros.php
@@ -102,8 +102,37 @@ $ataqueEvitado = htmlspecialchars($ataqueXSS);
 <p>Ataque evitado: <?= $ataqueEvitado ?></p>
 
 
+<h3>FILTER_SANITIZE_NUMBER_INT</h3>
+<?php 
+$idade = "Tenho 15 anos";
+$idade = filter_var($idade, FILTER_SANITIZE_NUMBER_INT);
+
+?>
+
+<p>Idade: <?= $idade ?></p>
 
 
+<h3>FILTER_SANITIZE_NUMBER_FLOAT</h3>
+
+
+<?php 
+$precoInicial = "R$ 1000";
+$desconto = "R$ 500.30";
+
+
+$precoInicial = filter_var($precoInicial, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+
+$desconto = filter_var($desconto, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+
+$precoFinal = $precoInicial - $desconto
+
+?>
+
+
+<p>Preço inicial: <?= $precoInicial ?></p>
+<p>Desconto de: <?= $desconto ?></p>
+<p>Preço final: <?= $precoFinal ?></p>
 
 
 </div>
