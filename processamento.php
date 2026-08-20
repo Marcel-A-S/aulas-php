6416fea551ef4f5ef5944ae55e85bcf1b3a3db3a
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-20 11:34:05 -0300

    Capturando os dados de cada campo

--- processamento.php
+++ processamento.php
@@ -1,38 +1,55 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Processamento</title>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 
 </head>
+
 <body>
 
-<div class="container">
+    <div class="container">
 
-<h1>Recebendo o processamento de dados</h1>
-<hr>
+        <h1>Recebendo o processamento de dados</h1>
+        <hr>
 
-<?php 
-var_dump($_POST);
-?>
+        <?php
+        //Capturando os dados de cada campo
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $idade = $_POST["idade"];
+        $mensagem = $_POST["mensagem"];
+        $interesses = $_POST["interesses"]; // Array
+        $informativos = $_POST["informativos"];
+        ?>
 
-</div>
+        <h2>Dados recebidos</h2>
+        <p>Nome: <?= $nome ?></p>
+        <p>E-mail: <?= $email ?></p>
+        <p>Idade: <?= $idade ?> anos</p>
+        <p>Mensagem: <?= $mensagem ?></p>
+        <p>Interesses: <?= implode(", ", $interesses) ?></p>
+        <p>Informativos: <?= $informativos ?></p>
+
+    </div>
 
 
 
 
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
 
 
 
 
-    
+
 </body>
+
 </html>
