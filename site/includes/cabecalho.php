ad0874f4b180aea00fd9621619855b3a437714ba
Marcel-A-S <marcelo_grl@yahoo.com.br>
2026-08-20 08:47:34 -0300

    adiciona but strep

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -1,8 +1,7 @@
-
-<?php 
+<?php
 // Definir um caminho de base/referência para os links
 const BASE = "/site/";
- ?>
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,11 +9,18 @@ const BASE = "/site/";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site usando PHP</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+
+
+
+
 </head>
 
 <body>
 
-    <header>
+    <header class="text-center">
 
         <h1>Site com PHP</h1>
 
@@ -24,11 +30,11 @@ const BASE = "/site/";
             <a href="<?= BASE ?>cursos.php">Cursos</a>
             <a href="<?= BASE ?>duvidas.php">Dúvidas</a>
             <a href="<?= BASE ?>planos.php">Planos</a>
-        <a href="<?= BASE ?>consultoria.php">Consultoria</a>
+            <a href="<?= BASE ?>consultoria.php">Consultoria</a>
 
 
         </nav>
     </header>
 
 
-    <main>
+    <main class="container">
